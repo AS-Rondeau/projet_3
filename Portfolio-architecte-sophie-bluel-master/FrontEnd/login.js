3a5cd927d03f3bf8da320fb79dfd82d169361342
AS-Rondeau <125301190+AS-Rondeau@users.noreply.github.com>
2023-03-12 18:14:57 +0100

    création fichier login.js
    fichier javascript créé avec : 
    appel à l'API pour chargement des identifiants
    création de la fonction verificationConnexion avec boucle et if/else
    
    modification fichier index.html avec création d'une div pour afficher le message d'erreur si échec de connexion

---
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
@@ -0,0 +1,27 @@
+//récupération de la liste des identifiants
+const reponse = await fetch("http://localhost:5678/api/users/login");
+const identifiants = await reponse.json();
+
+const messageErreur = document.getElementByClass("erreur");
+const formulaire = document.getElementById("formulaireConnexion");
+const boutonConnexion=document.getElementById("connexion");
+
+boutonConnexion.addEventListener("click", verificationconnexion);
+const verificationConnexion = (identifiant)=>{
+    const emailEntre = formulaire.identifiant.value;
+    const motDePasseEntre = formulaire.motdepasse.value;
+
+    for(let i = 0; i < identifiants.length; i++) {
+        const identifiant = identifiants[i];
+        const emailUtilisateur=identifiant.email;
+        const motDePasseUtilisateur=identifiant.password;
+        
+        if (emailEntre === "emailUtilisateur" && motDePasseEntre === "motDePasseUtilisateur") {
+            //aller sur la page d'accueil avec possibilité de modification de la galerie
+            document.location.replace("index.html");
+        } else {
+            //affichage du message d'erreur : constante "messageErreur"
+            messageErreur.style.display = block;
+        }
+    }
+}
