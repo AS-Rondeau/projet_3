16fbdb1386933ad4cd934d3947f365f412ea0ff1
AS-Rondeau <125301190+AS-Rondeau@users.noreply.github.com>
2023-03-13 15:15:39 +0100

    modification login
    fonction login via method post et non get

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/login.js
@@ -1,5 +1,51 @@
-//récupération de la liste des identifiants
-const reponse = await fetch("http://localhost:5678/api/users/login");
+const formulaire = document.getElementById("formulaireConnexion");
+
+const boutonConnexion=document.getElementById("connexion");
+
+const affichageFiltres=document.getElementByClass("filtres");
+const logout=document.getElementById("demande_logout");
+const login=document.getElementById("demande_login");
+const edition=document.getElementByClass("mode_edition");
+
+const messageErreur = document.getElementByClass("erreur");
+
+const emailEntre = formulaire.identifiant.value;
+const motDePasseEntre = formulaire.motdepasse.value;
+
+let utilisateur = {
+    "email": emailEntre,
+    "password": motDePasseEntre
+};
+  
+let response = await fetch("http://localhost:5678/api/users/login", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(utilisateur)
+  });
+  
+let result = await response.json();
+
+if(result.ok===true){
+    //aller sur la page d'accueil
+    document.location.replace("index.html");
+    //apparition de boutons d'action pour éditer le site + en-tête fond noir
+    edition.style.display=block;
+    //disparition des filtres
+    affichageFiltres.style.display=none;
+    //login devient logout dans header
+    login.style.display=none;
+    logout.style.display=block;
+} else {
+    //affichage du message d'erreur : constante "messageErreur"
+    messageErreur.style.display = block;
+}
+
+
+
+//si method GET pour récupérer la liste des identifiants et la comparer avec identifiants entrés
+/* const reponse = await fetch("http://localhost:5678/api/users/login");
 const identifiants = await reponse.json();
 
 const messageErreur = document.getElementByClass("erreur");
@@ -35,4 +81,4 @@ const verificationConnexion = (identifiant)=>{
             messageErreur.style.display = block;
         }
     }
-}
+} */
